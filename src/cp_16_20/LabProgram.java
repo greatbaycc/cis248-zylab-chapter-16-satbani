271492a2bee0fcbc7c008f3e4cf83b35c06dbe68
satbani <157083938+satbani@users.noreply.github.com>
2024-05-04 23:43:06 -0400

    Update LabProgram.java

--- src/cp_16_20/LabProgram.java
+++ src/cp_16_20/LabProgram.java
@@ -6,7 +6,13 @@ import java.util.Scanner;
 public class LabProgram {
 
    /* TODO: Write recursive reverseString() method here. */
-   
+   public static String reverseString(String str) {
+       if (str.isEmpty()) {
+            return str;
+        } else {
+            return str.charAt(str.length() - 1) + reverseString(str.substring(0, str.length() - 1));
+        }
+    }
    
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
