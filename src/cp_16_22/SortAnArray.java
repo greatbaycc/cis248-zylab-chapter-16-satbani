938f85d6eebe41d675a3cfb3c04c13d2224e5f52
satbani <157083938+satbani@users.noreply.github.com>
2024-05-04 23:56:04 -0400

    Update SortAnArray.java

--- src/cp_16_22/SortAnArray.java
+++ src/cp_16_22/SortAnArray.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class SortAnArray {
 
    /* Define your method here */
+   public static void sortArray(int[] myArr) {
+      for (int i = 0; i < myArr.length; i++) {
+         for (int j = 0; j < myArr.length - 1; j++) {
+            if (myArr[j] > myArr[j + 1]) {
+               int temp = myArr[j];
+               myArr[j] = myArr[j + 1];
+               myArr[j + 1] = temp;
+            }
+         }
+      }
+   }
 
    public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
